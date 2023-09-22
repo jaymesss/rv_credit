9c8ef82665559d2bc2c5a2e524c4085854adff9d
James <hoffmannjamesjr@gmail.com>
2023-09-21 22:50:50 -0700

    Create config.lua

---
+++ shared/config.lua
@@ -0,0 +1,26 @@
+Config = {}
+Config.CoreName = "qb-core"
+Config.JobName = "banker"
+Config.AllowDebt = true
+Config.TabletItem = 'banker_tablet'
+Config.TabletPrice = '500'
+Config.Information = {
+    Ped = {
+        Coords = vector4(147.87, -1041.73, 28.37, 339.13),
+        Model = 'a_m_m_hasjew_01'
+    },
+    Target = {
+        Coords = vector3(147.87, -1041.73, 29.37),
+        Heading = 160,
+        Label = "View Credit Information"
+    }
+}
+Config.TabletShop = {
+    Enabled = true,
+    Label = "Tablet Shop",
+    Target = {
+        Coords = vector3(143.53, -1041.89, 29.37),
+        Heading = 80,
+        Label = "Open Tablet Shop"
+    }
+}
