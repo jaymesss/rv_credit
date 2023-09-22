b37a3b987247c177c2d757ae3f3d605d2f2c9eac
James <hoffmannjamesjr@gmail.com>
2023-09-21 22:50:44 -0700

    Create en.lua

---
+++ locale/en.lua
@@ -0,0 +1,36 @@
+Locale = {}
+Locale.Error = {
+    player_not_found = 'A player could not be found with that ID',
+    cant_afford = 'The company cannot afford this loan.',
+    player_cant_afford = 'They do not have enough money in their bank for this loan!',
+    cant_pay = "You don't have enough money in your bank to pay your loan!",
+    none_active = 'You have no active loans.',
+    not_banker = 'Only bankers can access the tablet.'
+}
+Locale.Success = {
+    you_gave = 'You gave fullname a $amount loan!',
+    received = 'You received a $amount loan!',
+    paid_off = 'You just paid off $amount of your loan.'
+}
+Locale.Info = {
+    banker_menu = 'Banker Menu',
+    create_loan = 'Create New Loan',
+    create_loan_desc = 'Create a new loan for a person',
+    ongoing = 'Ongoing Loans',
+    ongoing_desc = 'View currently ongoing loans',
+    old_loans = 'Old Loans',
+    old_loans_desc = 'The history of our loans',
+    target_id = 'Target ID',
+    full_name = 'Full Name',
+    loan_name = 'Loan Name',
+    amount = 'Amount',
+    interest_rate = 'Interest Rate',
+    down_payment = 'Down Payment',
+    payment_amount = '# Of Payments',
+    notes = 'Loan Notes',
+    click_me = 'Click me for more information.',
+    opening_tablet = 'Opening Tablet...',
+    loan_details = 'Loan Details',
+    opening_information = 'Opening Credit Info...',
+    payments_left = 'Payments Left'
+}
